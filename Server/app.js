5312f29f7f0e633130bab097696fc52cd735a62b
Vinod Chandra <vinodchandra979@gmail.com>
2025-01-04 01:18:58 +0530

    update

--- Server/app.js
+++ Server/app.js
@@ -1,14 +1,13 @@
-import express from 'express'
+import express from 'express';
 import connectDB from './db/ConnectDB.js';
 import { APP_PORT, FRONTEND_URL, JWT_KEY, MONGO_URI } from './config/Index.js';
-import userRoutes from './routes/userRoutes.js'
-import workspaceRoutes from './routes/workspaceRoutes.js'
+import userRoutes from './routes/userRoutes.js';
+import workspaceRoutes from './routes/workspaceRoutes.js';
 import errorHandler from './middleware/errorHandlers.js';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import path from 'path';
 import { fileURLToPath } from 'url';
-
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -18,44 +17,40 @@ const PORT = APP_PORT;
 
 // middleware
 app.use(cors({
-    origin: FRONTEND_URL,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true
+  origin: FRONTEND_URL,
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true
 }));
 app.use(express.json());
-app.use(cookieParser(JWT_KEY))
+app.use(cookieParser(JWT_KEY));
 
-
-// Api Routes
+// API Routes
 app.use('/api/user', userRoutes);
 app.use('/api/workspace', workspaceRoutes);
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'client/build')));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'), (err) => {
-            if (err) {
-                res.status(500).send("Internal server error while serving static files.");
-            }
-        });
-    });
-}
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
+console.log(path.join(__dirname, 'client', 'build')); // Log to confirm path
 
 
 // Health Check Route
 app.get('/check', (req, res) => {
-    res.send('app is running');
+  res.send('app is running');
 });
 
 // Error Handling Middleware
 app.use(errorHandler);
 
-const start = async ()=>{
-    try {
-        await connectDB(MONGO_URI);
-        app.listen(PORT, ()=>console.log(`Server is running on port ${PORT}`));
-    } catch (error) {
-        console.log(error);
-    }
-}
+const start = async () => {
+  try {
+    await connectDB(MONGO_URI);
+    app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
 start();
